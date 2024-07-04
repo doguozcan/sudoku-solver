2f9fdb68dc0f17741e3e510a669265a5bce0525f
doguozcan <76822280+doguozcan@users.noreply.github.com>
2024-07-04 22:16:39 +0300

    Implement sudoku functionality

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,168 @@
 class SudokuSolver {
+  checkIsThereADot(puzzleString, row, column) {
+    // A -> 0
+    // B -> 1
+    // C -> 2
+    row = row.charCodeAt(0) - 65
 
-  validate(puzzleString) {
+    // 1 -> 0
+    // 2 -> 1
+    // 3 -> 2
+    column -= 1
+
+    // if the targeted place is dot
+    if (puzzleString[row * 9 + column] === '.') {
+      return true
+    }
+
+    // if it is empty
+    return false
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(puzzleString, row, value) {
+    // A -> 0
+    // B -> 1
+    // C -> 2
+    row = (row.charCodeAt(0) - 65) * 9
 
+    // check each box of the row
+    for (let i = 0; i < 9; i++) {
+      // if the box has the same value as the value that we want to place
+      if (puzzleString[row] === value) {
+        return false
+      }
+
+      row += 1
+    }
+
+    // it is available
+    return true
   }
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkColPlacement(puzzleString, column, value) {
+    // 1 -> 0
+    // 2 -> 1
+    // 3 -> 2
+    column -= 1
 
+    // check each box in the column
+    for (let i = 0; i < 9; i++) {
+      // if the current column has the same value as the one we want to place
+      if (puzzleString[column] === value) {
+        return false
+      }
+
+      column += 9
+    }
+
+    // available
+    return true
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    // A -> 0
+    // B -> 1
+    // C -> 2
+    // D -> 3
+    // E -> 4
+    // F -> 5
+    // G -> 6
+    row = row.charCodeAt(0) - 65
 
+    // 0 -> 0
+    // 1 -> 0
+    // 2 -> 0
+    // 3 -> 27
+    // 4 -> 27
+    // 5 -> 27
+    // 6 -> 54
+    // 7 -> 54
+    row = Math.floor(row / 3) * 27
+
+    // 1 -> 0
+    // 2 -> 1
+    // 3 -> 2
+    // 4 -> 3
+    // 5 -> 4
+    column = column - 1
+
+    // 0 -> 0
+    // 1 -> 0
+    // 2 -> 0
+    // 3 -> 3
+    // 4 -> 3
+    // 5 -> 3
+    // 6 -> 6
+    // 7 -> 6
+    // 8 -> 6
+    column = Math.floor(column / 3) * 3
+
+    let index = row + column
+
+    // check all boxes of region
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (puzzleString[index] === value) {
+          return false
+        }
+
+        index += 1
+      }
+
+      index += 6
+    }
+
+    return true
+  }
+
+  isValid(puzzleString, row, column, value) {
+    return (
+      this.checkRowPlacement(puzzleString, row, value) &&
+      this.checkColPlacement(puzzleString, column, value) &&
+      this.checkRegionPlacement(puzzleString, row, column, value)
+    )
+  }
+
+  findEmpty(puzzleString) {
+    for (let i = 0; i < 81; i++) {
+      if (puzzleString[i] === '.') {
+        let row = String.fromCharCode(65 + Math.floor(i / 9))
+        let column = (i % 9) + 1
+
+        return [row, column]
+      }
+    }
+
+    return null
   }
 
   solve(puzzleString) {
-    
+    let emptyBox = this.findEmpty(puzzleString)
+
+    if (!emptyBox) {
+      return puzzleString
+    }
+
+    let [row, column] = emptyBox
+
+    for (let value = 1; value < 10; value++) {
+      if (this.isValid(puzzleString, row, column, value.toString())) {
+        let index = (row.charCodeAt(0) - 65) * 9 + (column - 1)
+        puzzleString =
+          puzzleString.substring(0, index) +
+          value +
+          puzzleString.substring(index + 1)
+
+        let result = this.solve(puzzleString)
+
+        if (result) {
+          return result
+        }
+      }
+    }
+
+    return false
   }
 }
 
-module.exports = SudokuSolver;
-
+module.exports = SudokuSolver
--- routes/api.js
+++ routes/api.js
@@ -1,18 +1,111 @@
-'use strict';
+'use strict'
 
-const SudokuSolver = require('../controllers/sudoku-solver.js');
+const SudokuSolver = require('../controllers/sudoku-solver.js')
 
 module.exports = function (app) {
-  
-  let solver = new SudokuSolver();
+  let solver = new SudokuSolver()
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route('/api/check').post((req, res) => {
+    // get puzzle coordinate and value information from the body
+    const { puzzle, coordinate, value } = req.body
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
+    // if expected values are missing return an error message
+    if (!puzzle || !coordinate || !value) {
+      return res.json({ error: 'Required field(s) missing' })
+    }
 
-    });
-};
+    // if there are other than numbers and dot characters in the puzzle return an error
+    if (
+      puzzle.split('').filter((character) => /[^0-9.]/.test(character))
+        .length !== 0
+    ) {
+      return res.json({ error: 'Invalid characters in puzzle' })
+    }
+
+    // if the puzzle is not in 9x9 format
+    if (puzzle.length !== 81) {
+      return res.json({ error: 'Expected puzzle to be 81 characters long' })
+    }
+
+    // split the row and the column from coordiante
+    let [row, column] = coordinate.split('')
+
+    // if the value is not in the range 1 to 9
+    if (!'123456789'.includes(value)) {
+      return res.json({ error: 'Invalid value' })
+    }
+
+    // if row and column values are not in expected values
+    if (
+      !'ABCDEFGHI'.includes(row) ||
+      !'123456789'.includes(column) ||
+      coordinate.length !== 2
+    ) {
+      return res.json({ error: 'Invalid coordinate' })
+    }
+
+    column = parseInt(column)
+
+    if (!solver.checkIsThereADot(puzzle, row, column)) {
+      if (puzzle[(row.charCodeAt(0) - 65) * 9 + (column - 1)] === value) {
+        return res.json({ valid: true })
+      }
+    }
+
+    const rowAvailable = solver.checkRowPlacement(puzzle, row, value)
+    const columnAvailable = solver.checkColPlacement(puzzle, column, value)
+    const regionAvailable = solver.checkRegionPlacement(
+      puzzle,
+      row,
+      column,
+      value
+    )
+
+    let conflict = []
+
+    if (!rowAvailable) {
+      conflict.push('row')
+    }
+
+    if (!columnAvailable) {
+      conflict.push('column')
+    }
+
+    if (!regionAvailable) {
+      conflict.push('region')
+    }
+
+    if (conflict.length === 0) {
+      return res.json({ valid: true })
+    } else {
+      return res.json({ valid: false, conflict })
+    }
+  })
+
+  app.route('/api/solve').post((req, res) => {
+    const { puzzle } = req.body
+
+    if (!puzzle) {
+      return res.json({ error: 'Required field missing' })
+    }
+
+    if (
+      puzzle.split('').filter((character) => /[^0-9.]/.test(character))
+        .length !== 0
+    ) {
+      return res.json({ error: 'Invalid characters in puzzle' })
+    }
+
+    if (puzzle.length !== 81) {
+      return res.json({ error: 'Expected puzzle to be 81 characters long' })
+    }
+
+    const solution = solver.solve(puzzle)
+
+    if (solution) {
+      return res.json({ solution })
+    } else {
+      return res.json({ error: 'Puzzle cannot be solved' })
+    }
+  })
+}
